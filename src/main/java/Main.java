7e2e7a88e4ac276fc7da20f34b4c25d0885a5aab
Katarzyna Baranska <katarzyna.kinga.baranska@gmail.com>
2022-12-26 17:25:46 +0100

    Dodanie klas Employee, z danymi pracowników i Company, w której znajdują się wszystkie metody. Dodanie logiki do klasy Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -11,12 +12,29 @@ public class Main {
         if (file.exists()) {
             File stats = new File("stats.txt");
             stats.createNewFile();
-
-//            FileWriter fileWriter = new FileWriter(stats);
-//            fileWriter.write("Średnia wypłata: 5000");
-//
-//            fileWriter.close();
+            try (
+                    var fileWriter = new FileWriter(stats);
+                    var bufferedWriter = new BufferedWriter(fileWriter);
+            ) {
+                Employee[] employees = Company.readFile(file);
+                double averageSalary = Company.getAverageSalary(employees);
+                double minSalary = Company.getMinSalary(employees);
+                double maxSalary = Company.getMaxSalary(employees);
+                int[] employeesInDepartment = Company.getEmployeesInDepartment(employees);
+                bufferedWriter.write("Średnia wypłata: " + averageSalary);
+                bufferedWriter.newLine();
+                bufferedWriter.write("Minimalna wypłata: " + minSalary);
+                bufferedWriter.newLine();
+                bufferedWriter.write("Maksymalna wypłata: " + maxSalary);
+                bufferedWriter.newLine();
+                bufferedWriter.write("Liczba pracowników IT: " + employeesInDepartment[0]);
+                bufferedWriter.newLine();
+                bufferedWriter.write("Liczba pracowników Support: " + employeesInDepartment[1]);
+                bufferedWriter.newLine();
+                bufferedWriter.write("Liczba pracowników Management: " + employeesInDepartment[2]);
+            } catch (NumberFormatException e) {
+                System.err.println("Wypłata musi być liczbą");
+            }
         }
-
     }
 }
